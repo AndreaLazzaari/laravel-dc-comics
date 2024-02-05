7fffdd1d65b7269b9af75098c5b9aa72e3e6ba88
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-02-05 17:10:57 +0100

    creata in views cartella comics-guest + file index.blade.php + creato nuovo controller + inserita rotta per il controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+
+Route::get('/comics', [ComicController::class, 'index']);->name('comics.index');
+Route::get('/comics/{id}' , [ComicController::class, 'show']);->name('comics.show');
+
