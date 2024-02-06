f294ce66df9cc4084d9c6423d4dad3ee44ee787f
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-02-06 16:48:56 +0100

    inserite card in index.blade

--- resources/views/guest/comics/index.blade.php
+++ resources/views/guest/comics/index.blade.php
@@ -1,1 +1,25 @@
-aooo
+@extends('layouts.app')
+
+@section('main-content')
+    <section class="comics">
+        <div class="container">
+            <div class="row">
+                @foreach ($comics as $comic)
+
+                    <div class="col-2">
+                        <div class="card">
+                            <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    {{ $comic['series']}}
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+
+                @endforeach
+
+            </div>
+        </div>
+    </section>
+@endsection
