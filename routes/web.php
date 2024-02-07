fa5db0e10bd405d9643a6ea4ae739e5eb0877979
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-02-07 15:19:10 +0100

    creata rotta per edit

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
 Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
 Route::post('/comics/store', [ComicController::class, 'store'])->name('comics.store');
 Route::get('/comics/{comic}' , [ComicController::class, 'show'])->name(('comics.show'));
+Route::get('/comics/{comic}/edit' , [ComicController::class, 'edit'])->name(('comics.edit'));
+
 
